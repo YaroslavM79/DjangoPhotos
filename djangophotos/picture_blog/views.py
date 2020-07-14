c255a5b596d3a3ba977d9f638363560df76c05a8
Yaroslav <YMotalov@gmail.com>
2020-07-14 18:13:58 +0300

    added pagination to main screen.

--- djangophotos/picture_blog/views.py
+++ djangophotos/picture_blog/views.py
@@ -46,7 +46,7 @@ def user_logout(request):
 class HomePicturesView(ListView):
     model = PictureBlog  # по сути сделали news = News.objects.all()
     template_name = 'picture_blog/home_pictures_list.html'
-    context_object_name = 'Pictures'
+    context_object_name = 'pictures'
     queryset = PictureBlog.objects.all()
     paginate_by = 10
     def get_context_data(self, *, object_list=None, **kwargs):
